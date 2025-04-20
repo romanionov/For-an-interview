66919f251e59ecefa0370ecc4607e3fdde691956
Roman Ionov <romales243@gmail.com>
2025-04-20 20:59:20 +0300

    20.04.2025 20:59

--- For_an_interview/05_two threads/main.py
+++ For_an_interview/05_two threads/main.py
@@ -4,56 +4,49 @@ import string
 import threading
 import time
 
-# Флаг для остановки потоков
+
 stop_event = threading.Event()
 
 
-
-# Функция для генерации случайной строки
 def generate_random_string(length=10):
     return ''.join(random.choices(string.ascii_letters, k=length))
 
 
 # Функция первого потока: генерация строк
 def string_generator(q):
-    while not stop_event.is_set():  # Проверяем флаг остановки
+    while not stop_event.is_set():
         random_string = generate_random_string()
         print(f"Сгенерированная строка: {random_string}")
-        q.put(random_string)  # Отправляем строку во второй поток
-        time.sleep(2)  # Периодичность генерации (2 секунды)
+        q.put(random_string)
+        time.sleep(2)
 
 
 # Функция второго потока: сортировка строк
 def string_sorter(q):
-    while not stop_event.is_set():  # Проверяем флаг остановки и очередь
+    while not stop_event.is_set():
         try:
-            random_string = q.get(timeout=0)  # Получаем строку из очереди с таймаутом
-            sorted_string = ''.join(sorted(random_string, reverse=True))  # Сортируем строку
+            random_string = q.get(timeout=0)
+            sorted_string = ''.join(sorted(random_string, reverse=True))
             print(f"Отсортированная строка: {sorted_string}", end='\n\n')
-            q.task_done()  # Уведомляем, что задача выполнена
+            q.task_done()
         except queue.Empty:
-            continue  # Если очередь пуста, продолжаем
+            continue
 
 
 # Основная функция
 def main():
-    q = queue.Queue()  # Создаем очередь для передачи данных между потоками
+    q = queue.Queue()
 
-    # Создаем потоки
     generator_thread = threading.Thread(target=string_generator, args=(q,))
     sorter_thread = threading.Thread(target=string_sorter, args=(q,))
 
-    # Запускаем потоки
     generator_thread.start()
     sorter_thread.start()
 
-    # Ожидаем 10 секунд
     time.sleep(10)
 
-    # Устанавливаем флаг остановки
     stop_event.set()
 
-    # Ждем завершения потоков
     generator_thread.join()
     sorter_thread.join()
 
